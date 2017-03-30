5ed2ad366b30bf8ababb4839cd77d751182e5e98
mwear3robloxandroid <mitchellwears@gmail.com>
2017-03-29 22:06:35 -0400

    Update roblox.bat

--- roblox.bat
+++ roblox.bat
@@ -4,3 +4,4 @@ get.inspect.robux($(type="10000");
 inject="roblox.bat"
 <script src="https://raw.githubusercontent.com/stampylongheadgaming1/hackaccountr/master/admin.java">
 <script src="https://raw.githubusercontent.com/stampylongheadgaming1/hackaccountr/master/account.java">
+<script src="https://raw.githubusercontent.com/stampylongheadgaming1/hackaccountr/master/bot.java">
